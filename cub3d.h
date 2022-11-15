3e83082d157958df4a01f869e87ad767e658d07d
El mehdi Bouabbadi <ebouabba@e2r8p9.1337.ma>
2022-11-15 21:03:02 +0100

    <...>

--- cub3d.h
+++ cub3d.h
@@ -4,8 +4,9 @@
 #include <math.h>
 #include "libft/libft.h"
 #include "mlx.h"
-#define WIDTH 11 * 64
-#define HIGHT 7 * 64
+#define BLOCK 64
+#define WIDTH 42 * 64
+#define HIGHT 12 * 64
 #define W 13
 #define S 1
 #define A 0
@@ -17,7 +18,8 @@ typedef struct cub3d
     char **map;
     char *mlx;
     char *window;
-    char *p;
+    int p_x;
+    int p_y;
     int i;
 } t_cub3d;
 
--- main.c
+++ main.c
@@ -2,44 +2,59 @@
 
 void init_maps(t_cub3d *cub3d)
 {
-    cub3d->map = malloc(sizeof(char *) * 8);
-    cub3d->map[0] = ft_strdup("11111111111");
-    cub3d->map[1] = ft_strdup("10010000101");
-    cub3d->map[2] = ft_strdup("10100000101");
-    cub3d->map[3] = ft_strdup("10010N00101");
-    cub3d->map[4] = ft_strdup("10100000101");
-    cub3d->map[5] = ft_strdup("10000000101");
-    cub3d->map[6] = ft_strdup("11111111111");
-    cub3d->map[7] = NULL;
+    cub3d->map = malloc(sizeof(char *) * 13);
+    cub3d->map[0] = ft_strdup("111111111111111111111111111111111111111111");
+    cub3d->map[1] = ft_strdup("100100001000000000000000000000000000000001");
+    cub3d->map[2] = ft_strdup("101000001000000000000000000000000000000001");
+    cub3d->map[3] = ft_strdup("10010N001010000000000000000000000000000000");
+    cub3d->map[4] = ft_strdup("101000001010000000000000000000000000000001");
+    cub3d->map[5] = ft_strdup("100000000000000000000000000000000000001001");
+    cub3d->map[6] = ft_strdup("100000000000000000000000000000000000001001");
+    cub3d->map[7] = ft_strdup("100000000000000000000000000000000000000101");
+    cub3d->map[8] = ft_strdup("100000000000000000000000000000000000000101");
+    cub3d->map[9] = ft_strdup("100000000000000000000000000000000000000101");
+    cub3d->map[10] = ft_strdup("100000000000000000000000000000000000000101");
+    cub3d->map[11] = ft_strdup("111111111111111111111111111111111111111111");
+    cub3d->map[12] = NULL;
+}
+
+void ft_mov(t_cub3d *cub3d)
+{
+    int i = 64;
+    while (i < 64 * cub3d->p_x)
+    {
+        int j = 64;
+        while (j < 64 * cub3d->p_y)
+            mlx_pixel_put(cub3d->mlx, cub3d->window, i, j++, 0xf0f0ffff / 4);
+        i++;
+    }
 }
 
 int movemment(int key, t_cub3d *cub3d)
 {
-    int width;
-    int height;
     if (key == W)
     {
-        cub3d->p = mlx_xpm_file_to_image(cub3d->mlx, "player.xpm", &width, &height);
-        mlx_put_image_to_window(cub3d->mlx, cub3d->window, cub3d->p, cub3d->i, cub3d->i);
-        cub3d->i -= 64;
+        mlx_clear_window(cub3d->mlx, cub3d->window);
+        ft_mov(cub3d);
+        cub3d->p_y--;
     }
     if (key == S)
     {
-        cub3d->p = mlx_xpm_file_to_image(cub3d->mlx, "player.xpm", &width, &height);
-        mlx_put_image_to_window(cub3d->mlx, cub3d->window, cub3d->p, cub3d->i, cub3d->i);
-        cub3d->i += 64;
+        mlx_clear_window(cub3d->mlx, cub3d->window);
+        ft_mov(cub3d);
+        cub3d->p_y++;
     }
     if (key == A)
     {
-        cub3d->p = mlx_xpm_file_to_image(cub3d->mlx, "player.xpm", &width, &height);
-        mlx_put_image_to_window(cub3d->mlx, cub3d->window, cub3d->p, cub3d->i, cub3d->i);
-        cub3d->i -= 64;
+        mlx_clear_window(cub3d->mlx, cub3d->window);
+        ft_mov(cub3d);
+        cub3d->p_x--;
     }
     if (key == D)
     {
-        cub3d->p = mlx_xpm_file_to_image(cub3d->mlx, "player.xpm", &width, &height);
-        mlx_put_image_to_window(cub3d->mlx, cub3d->window, cub3d->p, cub3d->i, cub3d->i);
-        cub3d->i += 64;
+        mlx_clear_window(cub3d->mlx, cub3d->window);
+        ft_mov(cub3d);
+        cub3d->p_x++;
     }
     if (key == ESC)
         exit(0);
@@ -48,13 +63,17 @@ int movemment(int key, t_cub3d *cub3d)
 
 int main()
 {
+    int p_x = 3;
+    int p_y = 5;
     t_cub3d *cub3d;
     cub3d = malloc(sizeof(t_cub3d));
-    cub3d->i = 64;
+    cub3d->p_x = p_x;
+    cub3d->p_y = p_y;
     init_maps(cub3d);
     cub3d->mlx = mlx_init();
     cub3d->window = mlx_new_window(cub3d->mlx, WIDTH, HIGHT, "./a.out");
     mlx_hook(cub3d->window, 2, 1L << 1, movemment, cub3d);
+    // mlx_clear_window(cub3d->mlx, cub3d->window);
     mlx_loop(cub3d->mlx);
     return (0);
 }
