b5857582a73ff7b24ffaef2e2909ff9a606b9848
zmoussam <zakariamoussammi87@gmail.com>
2023-03-01 00:47:24 +0100

    makefile done

--- bonus/include/parsing.h
+++ bonus/include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: zmoussam <zmoussam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:01:19 by mmakboub          #+#    #+#             */
-/*   Updated: 2023/02/28 19:47:48 by zmoussam         ###   ########.fr       */
+/*   Updated: 2023/03/01 00:45:46 by zmoussam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,7 @@ typedef struct s_index
 	size_t j;
 }t_index;
 
-typedef struct s_cordinates
-{
-	double			x;
-	double			y;
-}					t_cordinates;
-
-typedef struct s_abtmap
+typedef struct s_map_info
 {
 	char			*no;
 	char			*so;
@@ -51,46 +45,46 @@ typedef struct s_abtmap
 	int				lenghtlines;
 	int				has_f;
 	int				has_c;
-	int				floor;
-	int				ceilling;
+	int				c_floor;
+	int				c_ceilling;
 	int				lineindex;
 	size_t				maxlenmap;
 
-}					t_abtmap;
+}					t_map_info;
 
 char				*remove_caract(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 void				ft_check_cub(char *filename);
 int					countline(char *file);
-int					ft_reading_maps(t_abtmap *jeu, char *file);
+int					ft_reading_maps(t_map_info *jeu, char *file);
 char				**remplir_tableau(void);
-void				initializer(t_abtmap *game);
+void				initializer(t_map_info *game);
 void				free_tab(char **tab, int size);
-int					check_intern_map(char *clean_ptr, t_abtmap *game);
-void				parse_direction(t_abtmap *game, char *line);
-void				ea_direct(t_abtmap *game, char *line);
-void				no_direct(t_abtmap *game, char *line);
-void				we_direct(t_abtmap *game, char *line);
-void				so_direct(t_abtmap *game, char *line);
+int					check_intern_map(char *clean_ptr, t_map_info *game);
+void				parse_direction(t_map_info *game, char *line);
+void				ea_direct(t_map_info *game, char *line);
+void				no_direct(t_map_info *game, char *line);
+void				we_direct(t_map_info *game, char *line);
+void				so_direct(t_map_info *game, char *line);
 char				*ft_strcpy(char *dest, char *src);
 char				*remove_prefix(char *line, char *prefix);
 void				memory_error(void);
-void				parse_color(t_abtmap *game, char *cleanline);
+void				parse_color(t_map_info *game, char *cleanline);
 int					receive_rgb_color(char *color);
 int					isalldigits(const char *s);
 int					rgb_converter(char **rgbclr);
-int					parse_map(t_abtmap *game, char *first_line, int fd);
+int					parse_map(t_map_info *game, char *first_line, int fd);
 int					valid_n(char **map, size_t i, size_t j);
 int					valid_e(char **map, size_t i, size_t j);
-int					valid_s(char **map, size_t i, size_t j, t_abtmap *game);
+int					valid_s(char **map, size_t i, size_t j, t_map_info *game);
 int					valid_w(char **map, size_t i, size_t j);
-int					check_valid_map(t_index index, char **map, t_abtmap *game, int *counter);
-int					checkmap(t_abtmap *game);
-int					check_path(t_abtmap *game, char *cleanline);
-void				check_retvalue(int retvalue, char *clean_ptr, t_abtmap *game);
-int 				base_parsing(char *file, t_abtmap *game);
-void 				findmaxline(t_abtmap *game);
-char 				*fillwithspace(char *line, t_abtmap *game);
+int					check_valid_map(t_index index, char **map, t_map_info *game, int *counter);
+int					checkmap(t_map_info *game);
+int					check_path(t_map_info *game, char *cleanline);
+void				check_retvalue(int retvalue, char *clean_ptr, t_map_info *game);
+int 				base_parsing(char *file, t_map_info *game);
+void 				findmaxline(t_map_info *game);
+char 				*fillwithspace(char *line, t_map_info *game);
 char 				*onlyspaces(int diff);
 int 				numberofgamma(char *pathcolor);
 int 				checkgamma(char *pathcolor);
